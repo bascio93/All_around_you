ad5bbcf20249599acc5a6da4fb9ac2d36249a564
Alessandro Basciani <basciani.93@gmail.com>
2018-10-01 14:25:55 +0200

    aggiunto codice login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180927143933) do
+ActiveRecord::Schema.define(version: 20181001121513) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
