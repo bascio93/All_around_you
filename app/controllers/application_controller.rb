5a7a82a5e5e365d168c21f44980f8cdeaa768ffe
Alessandro Basciani <basciani.93@gmail.com>
2018-10-01 15:10:05 +0200

    password codifica

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+    def show
+    @user = User.find(params[:id])
+    debugger
+  end
+
+  def new
+  end
   protect_from_forgery with: :exception
   include SessionsHelper
 end
