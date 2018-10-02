a9fb31e2b6584876af375777fbd54851a1387abc
Ingegneria Informatica e Automatica <Overkill95>
2018-10-02 22:31:07 +0200

    Login + cookies added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user=User.find_by(email: params[:sessions][:email].downcase)
     if user && user.authenticate(params[:session][:password])
     log_in(user)
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
     redirect_to user
     else
         flash.now[:danger]= "Errore, user non esistente o password sbagliata, riprova il login perfavore"
@@ -12,5 +13,7 @@ class SessionsController < ApplicationController
     end
   end
   def destroy
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,11 +6,27 @@ module SessionsHelper
   def currentuser
     if @current_user==nil
         @current_user=User.find_by(id: session[:user_id])
-    end
     else
         return @current_user
     end
+  end
    def islogged
       !current_user.nil?
+   end
+   #Salva il token e lo user id criptato sul browser
+   def remember(user)
+      user.remember
+      cookies.permanent.signed[:user_id]=user.id
+      cookies.permanent[:remember_token]=user.remember_token
+   end
+   def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+   end
+   def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
    end
   end
