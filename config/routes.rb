bedeb72fda76fdcd2b493c2230da0adc3a45f1a4
Ingegneria Informatica e Automatica <RicDib>
2018-10-05 18:16:33 +0200

    modifiche pagine statiche

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+
   get 'sessions/new'
   root                'static_pages#home'
   get    'help'    => 'static_pages#help'
