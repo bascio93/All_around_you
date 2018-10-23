0de6918852a01d494f22b9e9eff6832d18412dab
PGrecoo <pasquale.alex10@gmail.com>
2018-10-23 12:25:17 +0200

    helper fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-
+# note: require 'devise' after require 'rspec/rails'
 require 'devise'
 require_relative 'spec_helper'
 
